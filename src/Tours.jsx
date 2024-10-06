edcfd5e1d2fccaf21d734772931dff3087a97823
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2024-10-06 17:28:56 +0100

    set up Tours component

--- src/Tours.jsx
+++ src/Tours.jsx
@@ -1,4 +1,18 @@
-const Tours = () => {
-  return <div>Tours</div>;
+import Tour from "./Tour";
+
+const Tours = ({ tours }) => {
+  return (
+    <section>
+      <div className="title">
+        <h3>Our Tours</h3>
+        <div className="title-underline"></div>
+      </div>
+      <div className="tours">
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} />;
+        })}
+      </div>
+    </section>
+  );
 };
 export default Tours;
