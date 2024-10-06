75728d84e94d317765f6e64397b63c36bd77a0b6
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2024-10-06 18:17:08 +0100

    re-fetch tours when none left

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,24 @@ const App = () => {
     );
   }
 
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No Tours Remaining</h2>
+          <button
+            type="button"
+            style={{ marginTop: "2rem" }}
+            className="btn"
+            onClick={() => fetchTours()}
+          >
+            Get More Tours
+          </button>
+        </div>
+      </main>
+    );
+  }
+
   return (
     <main>
       <Tours tours={tours} removeTour={removeTour} />
